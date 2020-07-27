7745faa44a7eae2a49090c612ce4a2466296bde0
Ryan <ryanmcg86@users.noreply.github.com>
2020-07-27 16:15:28 -0400

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -9,136 +9,138 @@ package csc120;
 import java.util.Scanner;
 
 public class BlackJack {
-	
 	ProgramThree title = new ProgramThree();
-	
-	private Player dealer;				//-player1 : Player		Player 1
-	
-	private Player n;					//-player2 : Player		Player 2
-	
-	private Deck deck;					//-deck : Deck			Deck of Cards
-	
+	//Player 1
+	private Player dealer;
+	//Player 2
+	private Player n;
+	private Deck deck;
 	int i;
-	
-	int myWins = 0;
-	
-	int dealerWins = 0;
-	
-	int ties = 0;
-	
-	int currentStreak = 0;
-	
-	int bestStreak = 0;
-	
-	int dealerStreak = 0;
-	
+	int myWins           = 0;
+	int dealerWins       = 0;
+	int ties             = 0;
+	int currentStreak    = 0;
+	int bestStreak       = 0;
+	int dealerStreak     = 0;
 	int dealerBestStreak = 0;
 	
-	
-	public BlackJack(){					//+BlackJack()			Create a deck
-	deck = new Deck();	
+	//Create a deck
+	public BlackJack(){
+		deck = new Deck();	
 	}
 	
-	public void playGame(){				//+playGame():void		-Create 2 Players
-	
+	//Create a playGame function
+	public void playGame(){
+		//Create 2 Players
 		dealer = new Player();
+		n      = new Player();
 		
-		n = new Player();
 		String name = null;
 		n.setName(name);
 		
-		deck.shuffle();					//						-Shuffle the deck
-	
+		//Shuffle the deck
+		deck.shuffle();
 	
 		String yesorNo = null;
 	
 		{
-		Card temp = deck.deal();		//			-Deal 2 Cards to each Player
-		dealer.addCard(temp);
-			
-		temp = deck.deal();
-		n.addCard(temp);
-			
-		temp = deck.deal();
-		dealer.addCard(temp);
-			
-		temp = deck.deal();
-		n.addCard(temp);
-		}
-			
-		
-	
-	System.out.println(n.toString());	//			-Display the player's info
-	
-	System.out.println("The Dealer's first card is a "//-Display the dealer's first
-			+ dealer.getCard(0));	//					 card
-	
-	if(n.getPoints() == 21 && n.getHand().getCount() == 2){
-		gameResults();
-		}
-	else if((dealer.getPoints() == 21) && (dealer.getHand().getCount() == 2)){
-		gameResults();
-	}
-	
-	else{
-	System.out.println("Would you like to hit? Y/N.");//-Permit the player to hit
-		Scanner scan = new Scanner(System.in);		//	until player stands or busts		 
-		yesorNo = scan.next();
-		while((yesorNo.equalsIgnoreCase("y") == false) && 
-				(yesorNo.equalsIgnoreCase("n") == false)){
-			System.out.println("That's not Y or N, I'll ask again;");
-			System.out.println("Would you like to hit?");
-			yesorNo = scan.next();
-		}
-		
-		
-		while(yesorNo.equalsIgnoreCase("n") == false
-				&& yesorNo.equalsIgnoreCase("y")){
-	
+		//Deal 2 Cards to each Player	
 		Card temp = deck.deal();
+		dealer.addCard(temp);
+			
+		temp = deck.deal();
 		n.addCard(temp);
-			if(n.getPoints() >= 21){
-				System.out.println(n.toString());
-				yesorNo = "no";
-				}
+			
+		temp = deck.deal();
+		dealer.addCard(temp);
+			
+		temp = deck.deal();
+		n.addCard(temp);
+		}
+				
+		//Display the player's info
+		System.out.println(n.toString());		
+	
+		//Display the dealer's first card
+		System.out.println("The Dealer's first card is a " + dealer.getCard(0));
+	
+		if(n.getPoints() == 21 && n.getHand().getCount() == 2){
+			gameResults();
+		}
+		else if((dealer.getPoints() == 21) && (dealer.getHand().getCount() == 2)){
+			gameResults();
+		}
+	
 		else{
-			System.out.println(n.toString());
+			//Permit the player to hit
 			System.out.println("Would you like to hit? Y/N.");
+			
+			//until player stays or busts...
+			Scanner scan = new Scanner(System.in);				 
 			yesorNo = scan.next();
-			while((yesorNo.equalsIgnoreCase("y") == false) && 
-					(yesorNo.equalsIgnoreCase("n") == false)){
+			
+			while((yesorNo.equalsIgnoreCase("y") == false) && (yesorNo.equalsIgnoreCase("n") == false)){
 				System.out.println("That's not Y or N, I'll ask again;");
-				System.out.println("Would you like to play again?");
+				System.out.println("Would you like to hit?");
 				yesorNo = scan.next();
 			}
-				if(n.getPoints() > 21){
+		
+			//As long as the player wants to hit...
+			while(yesorNo.equalsIgnoreCase("n") == false && yesorNo.equalsIgnoreCase("y")){
+		
+				//Deal a card
+				Card temp = deck.deal();
+				n.addCard(temp);
+					//And check again if the player wants to hit
+				if(n.getPoints() >= 21){
 					System.out.println(n.toString());
+					yesorNo = "no";
+				}
+				else{
+					System.out.println(n.toString());
+					System.out.println("Would you like to hit? Y/N.");
+					yesorNo = scan.next();
+					while((yesorNo.equalsIgnoreCase("y") == false) && (yesorNo.equalsIgnoreCase("n") == false)){
+						System.out.println("That's not Y or N, I'll ask again;");
+						System.out.println("Would you like to play again?");
+						yesorNo = scan.next();
 					}
+					if(n.getPoints() > 21){
+						System.out.println(n.toString());
 				}
 			}
-	if(n.getPoints() <= 21){
-	for(i = 0; i < dealer.getHand().getCount(); i++){//-Dealer's turn (if necessary)
-		if(dealer.getHand().getCard(i).getRank() > 11){
-			if((dealer.getHand().evaluate() <= 17)){ 
-	Card temp = deck.deal();//						 	see rules on page 1
-	dealer.addCard(temp);//							-Dealer must stand on 17 or more
 		}
-	}								//				-Dealer must hit on soft 17
-	else if(dealer.getHand().evaluate() < 17){
-	Card temp = deck.deal();
-	dealer.addCard(temp);
+		if(n.getPoints() <= 21){
+			//Dealer's turn (if necessary)
+			for(i = 0; i < dealer.getHand().getCount(); i++){
+				if(dealer.getHand().getCard(i).getRank() > 11){
+					if((dealer.getHand().evaluate() <= 17)){ 
+						//see rules on page 1
+						Card temp = deck.deal();
+						//Dealer must stand on 17 or more
+						dealer.addCard(temp);
+					}
+				}								
+				else if(dealer.getHand().evaluate() < 17){
+					Card temp = deck.deal();
+					dealer.addCard(temp);
+				}
+			} 
+		}
+		//Display game results
+		gameResults();
 	}
-	} 
-	}
-	gameResults();					//				-Display game results
-	}
-	}
+}
+	
 	public int gameWin(){
 		int DPoints = dealer.getPoints();
 		int PPoints = n.getPoints();
-		if(PPoints == 21 && n.getHand().getCount() == 2){//blackjack
+		
+		//blackjack
+		if(PPoints == 21 && n.getHand().getCount() == 2){
 			return 2;
 		}
+		//dealer wins, no tie.
 		else if(
 				((DPoints > PPoints) && (DPoints <= 21)) ||
 				((PPoints > 21) && (DPoints <= 21)) || 
@@ -146,142 +148,133 @@ public class BlackJack {
 				(dealer.getHand().getCount() == 2) && 
 				(n.getHand().getCount() != 2))
 				){
-			return 0;									//dealer wins, no tie.
+			return 0;									
 		}
+		//player wins, no tie.
 		else if(((DPoints < PPoints) && (PPoints <= 21)) || 
 				(DPoints > 21 && PPoints <= 21)){
-			return 1;									//player wins, no tie.
+			return 1;									
 		}
+		//tie
 		else if(((DPoints == PPoints) && (DPoints <= 21)) &&
 				(n.getHand().getCount() != 2)){
-			return 3;									//tie
+			return 3;									
 		}
 		return 4;
-		}
-	public void gameResults(){		//+gameResults():void		-Compare the hands
+	}
+	
+	//Compare the hands
+	public void gameResults(){		
 		System.out.println("Game over...");
 		System.out.println(" ");
+		String line = "======================================================="
+		String spaces = '                     '
 		int DPoints = dealer.getPoints();
 		int PPoints = n.getPoints();
-		if((DPoints > PPoints) 
-				&& (DPoints <= 21)){
-			System.out.println("                     Game Results");
-			System.out.println("====================================" +
-					"===================");
-									// 							-Display the winner
-			System.out.println("                     The Dealer won.");
+		//Display the winner
+		if((DPoints > PPoints) && (DPoints <= 21)){
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "The Dealer won.");
 			dealerWins++;
 			currentStreak = 0;
 			dealerStreak++;
 		}
 		else if((DPoints == PPoints) && (DPoints <= 21)){
-			System.out.println("                     Game Results");
-			System.out.println("=====================================" +
-					"==================");
-			System.out.println("                     You tied.");
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "You tied.");
 			ties++;
 		}
 		else if(PPoints == 21 && n.getHand().getCount() == 2){
-			System.out.println("                     Game Results");
-			System.out.println("======================================" +
-					"=================");
-			System.out.println("                     BLACKJACK!");
-			System.out.println("                     YOU WON!!!");
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "BLACKJACK!");
+			System.out.println(spaces + "YOU WON!!!");
 			myWins++;
 			currentStreak++;
 			dealerStreak = 0;
 			}
-		else if((DPoints < PPoints) 
-					&& (PPoints <= 21)){
-				System.out.println("                     Game Results");
-				System.out.println("===================================" +
-						"====================");
-				System.out.println("                     YOU WON!!!");
-				myWins++;
-				currentStreak++;
-				dealerStreak = 0;
-			}
-			else if(DPoints > 21 && PPoints <= 21){
-				System.out.println("                     Game Results");
-				System.out.println("===================================" +
-						"====================");
-				System.out.println("                     The Dealer busted.");
-				System.out.println("                     YOU WON!!!");
-				myWins++;
-				currentStreak++;
-				dealerStreak = 0;
+		else if((DPoints < PPoints) && (PPoints <= 21)){
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "YOU WON!!!");
+			myWins++;
+			currentStreak++;
+			dealerStreak = 0;
 		}
-			else if(PPoints > 21 && DPoints <= 21){
-				System.out.println("                     Game Results");
-				System.out.println("====================================" +
-						"===================");
-				System.out.println("                     You busted.");
-				System.out.println("                     The Dealer won.");
-				dealerWins++;
-				currentStreak = 0;
-				dealerStreak++;
-			}
-			else if(DPoints > 21 && PPoints > 21){
-				System.out.println("                     Game Results");
-				System.out.println("=====================================" +
-						"==================");
-				System.out.println("                     Both players busted... " +
-						"play again!");
-				}
-			else{
-				System.out.println("Error");
-				System.out.println(" ");
-			}
-		if(currentStreak > 0){//					-establishes the value
-			if(bestStreak <= currentStreak){//		 of the players best streak
+		else if(DPoints > 21 && PPoints <= 21){
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "The Dealer busted.");
+			System.out.println(spaces + "YOU WON!!!");
+			myWins++;
+			currentStreak++;
+			dealerStreak = 0;
+		}
+		else if(PPoints > 21 && DPoints <= 21){
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "You busted.");
+			System.out.println(spaces + "The Dealer won.");
+			dealerWins++;
+			currentStreak = 0;
+			dealerStreak++;
+		}
+		else if(DPoints > 21 && PPoints > 21){
+			System.out.println(spaces + "Game Results");
+			System.out.println(line);
+			System.out.println(spaces + "Both players busted... play again!");
+		}
+		else{
+			System.out.println("Error");
+			System.out.println(" ");
+		}
+		
+		//establishes the value of the players best streak
+		if(currentStreak > 0){
+			if(bestStreak <= currentStreak){
 				bestStreak = currentStreak;
 			}
 		}
-		if(dealerStreak > 0){//						-establishes the value
-			if(dealerBestStreak <= dealerStreak){//	 of the dealers best streak
+		//establishes the value of the dealers best streak
+		if(dealerStreak > 0){
+			if(dealerBestStreak <= dealerStreak){
 				dealerBestStreak = dealerStreak;
 			}
 		}
 		
 		System.out.println(" ");
 		
-		System.out.println(dealer.toString());//			-Display Player info
-		System.out.println(n.toString());//					 for all players
+		//Display Player info for all players
+		System.out.println(dealer.toString());
+		System.out.println(n.toString());
 		
 		System.out.println(" ");
 		
-		System.out.println("                     Record: " + myWins 
-				+ "-" + dealerWins + "-" + ties);
+		System.out.println(spaces + "Record: " + myWins + "-" + dealerWins + "-" + ties);
 		
 		System.out.println(" ");
 		
 		if(currentStreak > 0){
-		if(currentStreak == 1){
-			System.out.println("Current Winning Streak: " + currentStreak 
-					+ " game in a row.");
-		}
-		else{ 
-			System.out.println("Current Winning Streak: " + currentStreak 
-					+ " games in a row.");
-		}
+			if(currentStreak == 1){
+				System.out.println("Current Winning Streak: " + currentStreak + " game in a row.");
+			}
+			else{ 
+				System.out.println("Current Winning Streak: " + currentStreak + " games in a row.");
+			}
 		}
 		else{
-		if(dealerStreak == 1){
-			System.out.println("Dealers' Current Winning Streak: " + dealerStreak 
-					+ " game in a row.");
+			if(dealerStreak == 1){
+				System.out.println("Dealers' Current Winning Streak: " + dealerStreak + " game in a row.");
+			}
+			else{	
+				System.out.println("Dealers' Current Winning Streak: " + dealerStreak + " games in a row.");
+			}
 		}
-		else{	
-		System.out.println("Dealers' Current Winning Streak: " + dealerStreak 
-				+ " games in a row.");
-		}
-		}
-		System.out.println("===========================================" +
-				"============");
+		System.out.println(line);
 		
 		deck.initialize();
 		deck.shuffle();
-		}
+	}
 }
-
-
-
